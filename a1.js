11b96a98e02b1206e7348929b8982515792a12fb
himanshu850 <himanshupparmar824@gmail.com>
2024-05-17 17:38:32 -0400

    first commit

---
+++ a1.js
@@ -0,0 +1,25 @@
+/*********************************************************************************
+* WEB700 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Himanshu Parmar Student ID: 146708235  Date: 5/17/2024
+*
+********************************************************************************/
+
+
+serverVerbs = [GET,GET,GET,POST,GET,POST];
+serverPaths = [/,/about,/contact,/login,/panel,/logout];
+serverResponses = ["Welcome to WEB700 Assignment 1","This assignment was prepared by Himanshu",
+"Himanshu Parmar: hpparmar1@myseneca.ca","User Logged In","Main Panel","Logout Complete"];
+
+
+function httpRequest(httpVerb,path){
+    var count = serverPaths.length;
+    var code = 200;
+    for(var i=0; i > count; i++){
+
+    }
+    return code
+}

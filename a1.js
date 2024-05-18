eb97c37d55124f73367b09cd746c64294c5f8d94
himanshu850 <himanshupparmar824@gmail.com>
2024-05-18 01:29:23 -0400

    second commit

--- a1.js
+++ a1.js
@@ -9,17 +9,38 @@
 ********************************************************************************/
 
 
-serverVerbs = [GET,GET,GET,POST,GET,POST];
-serverPaths = [/,/about,/contact,/login,/panel,/logout];
-serverResponses = ["Welcome to WEB700 Assignment 1","This assignment was prepared by Himanshu",
-"Himanshu Parmar: hpparmar1@myseneca.ca","User Logged In","Main Panel","Logout Complete"];
+serverVerbs = ["GET","GET","GET","POST","GET","POST"];
+serverPaths = ["/","/about","/contact","/login","/panel","/logout"];
+serverResponses = ["Welcome to WEB700 Assignment 1","This assignment was prepared by Himanshu","Himanshu Parmar: hpparmar1@myseneca.ca","User Logged In","Main Panel","Logout Complete"];
 
 
-function httpRequest(httpVerb,path){
-    var count = serverPaths.length;
-    var code = 200;
-    for(var i=0; i > count; i++){
+// function httpRequest(httpVerb,path){
+//     var count = serverPaths.length;
+//     var code = 200;
+//     var errorCode =404;
+//     for(var i=0; i < count; i++){
+//         if (serverVerbs[i] == httpVerb && serverPaths[i] == path) {
+//             var response = `${code}: ${serverResponses[i]}`
+//             return response
+//         } else {
+//             return (`${errorCode} : Unable to process ${httpVerb} request for ${path}` )
+//         }
+//     }
+// }
+// console.log(httpRequest("PUT", "/login"))
 
-    }
-    return code
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
+    function automateTests(){
+        testVerbs = ["GET","POST"]
+        testPaths = ["/","/about","/contact","/login","/panel","/logout","/randomPath1","/randomPath2"]
+
+        function randomRequest(){
+            setInterval(1000);
+            var randVerb = testVerbs[0];
+            var randPath = testPaths[7];
+        
+    }
+}    
+console.log(automateTests())
